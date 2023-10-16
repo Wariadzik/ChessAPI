efb4a289fa7a194b87c8054720f9ca9f33b2c902
jkaczmarek <jkaczmarek@psmm.pl>
2023-10-16 17:25:22 +0200

    Add project files.

---
+++ ChessAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChessAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IWeatherForecastService _service;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var result = _service.Get();
+            return result;
+        }
+    }
+}
---
+++ ChessAPI/IWeatherForecastService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ChessAPI
+{
+    public interface IWeatherForecastService
+    {
+        IEnumerable<WeatherForecast> Get();
+    }
+}
